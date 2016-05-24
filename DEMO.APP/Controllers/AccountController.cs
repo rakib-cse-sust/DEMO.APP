62c371808c222951317cc97a6a005a7c622a8bd1
rakib-cse-sust <rakib.cse.sust@gmail.com>
2016-05-25 01:41:47 +0600

    commit

---
+++ DEMO.APP/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace DEMO.APP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryAngular]
+        public ActionResult Signin(FormCollection fc)
+        {
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        public ActionResult Signout()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ DEMO.APP/Modules/Account/Account.module.js
@@ -0,0 +1,16 @@
+﻿var accountApp = angular.module('accountApp', ['ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/signin');
+
+    $stateProvider
+    .state('signin', {  
+        url: '/',
+        templateUrl: 'Modules/Account/partials/partial-signin.html'
+    })
+    .state('registration', {
+        url: "/registration",
+        templateUrl: 'Modules/Account/partials/partial-registration.html'
+    });
+}]);

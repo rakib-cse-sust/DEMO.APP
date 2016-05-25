9daed26efb6f7a21738898887dad0ad45cd380e1
rakib-cse-sust <rakib.cse.sust@gmail.com>
2016-05-26 00:53:59 +0600

    gui

--- DEMO.APP/Modules/Administration/RoleSetup/controllers/RoleSetupController.js
+++ DEMO.APP/Modules/Administration/RoleSetup/controllers/RoleSetupController.js
@@ -15,8 +15,11 @@
         activate();
 
         function activate() {
-            vm.RoleList = [{ 'Id': 1, 'Name': 'Admin' }, { 'Id': 2, 'Name': 'User admin' },
-                { 'Id': 3, 'Name': 'Manager' }, { 'Id': 4, 'Name': 'Developer' }, { 'Id': 5, 'Name': 'Anonymous' }];
+            vm.RoleList = [{ 'Id': 1, 'Name': 'Admin', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName11' }, { 'Id': 2, 'ScreenName': 'ScreenName12' }, { 'Id': 3, 'ScreenName': 'ScreenName13' }] },
+{ 'Id': 2, 'Name': 'User admin', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName21' }, { 'Id': 2, 'ScreenName': 'ScreenName22' }, { 'Id': 3, 'ScreenName': 'ScreenName23' }] },
+{ 'Id': 3, 'Name': 'Manager', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName31' }] },
+{ 'Id': 4, 'Name': 'Developer', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName41' }, { 'Id': 2, 'ScreenName': 'ScreenName42' }, { 'Id': 3, 'ScreenName': 'ScreenName43' }] },
+{ 'Id': 5, 'Name': 'Anonymous', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName51' },  { 'Id': 3, 'ScreenName': 'ScreenName53' }] }];
         }
     }
 })();

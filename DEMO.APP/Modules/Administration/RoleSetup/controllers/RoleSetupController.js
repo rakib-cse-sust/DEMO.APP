1f3b3f40ceaa43e168b772a8a359c61eb44fee18
rakib-cse-sust <rakib.cse.sust@gmail.com>
2016-05-26 03:10:55 +0600

    boot card

--- DEMO.APP/Modules/Administration/RoleSetup/controllers/RoleSetupController.js
+++ DEMO.APP/Modules/Administration/RoleSetup/controllers/RoleSetupController.js
@@ -15,11 +15,186 @@
         activate();
 
         function activate() {
-            vm.RoleList = [{ 'Id': 1, 'Name': 'Admin', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName11' }, { 'Id': 2, 'ScreenName': 'ScreenName12' }, { 'Id': 3, 'ScreenName': 'ScreenName13' }] },
-{ 'Id': 2, 'Name': 'User admin', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName21' }, { 'Id': 2, 'ScreenName': 'ScreenName22' }, { 'Id': 3, 'ScreenName': 'ScreenName23' }] },
-{ 'Id': 3, 'Name': 'Manager', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName31' }] },
-{ 'Id': 4, 'Name': 'Developer', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName41' }, { 'Id': 2, 'ScreenName': 'ScreenName42' }, { 'Id': 3, 'ScreenName': 'ScreenName43' }] },
-{ 'Id': 5, 'Name': 'Anonymous', 'ModuleList': [{ 'Id': 1, 'ScreenName': 'ScreenName51' },  { 'Id': 3, 'ScreenName': 'ScreenName53' }] }];
+            vm.RoleList = [
+  {
+      "Id": 1,
+      "Name": "Admin",
+      "ModuleList": [
+        {
+            "Id": 1,
+            "ModuleName": "ModuleName11",
+            "ScreenList": [
+              {
+                  "Id": 1,
+                  "ScreenName": "ScreenName11"
+              },
+              {
+                  "Id": 2,
+                  "ScreenName": "ScreenName12"
+              },
+              {
+                  "Id": 3,
+                  "ScreenName": "ScreenName13"
+              },
+              {
+                  "Id": 4,
+                  "ScreenName": "ScreenName14"
+              },
+              {
+                  "Id": 5,
+                  "ScreenName": "ScreenName15"
+              },
+              {
+                  "Id": 6,
+                  "ScreenName": "ScreenName16"
+              }
+            ]
+        },
+        {
+            "Id": 2,
+            "ModuleName": "ModuleName12",
+            "ScreenList": [
+              {
+                  "Id": 1,
+                  "ScreenName": "ScreenName11"
+              },
+              {
+                  "Id": 2,
+                  "ScreenName": "ScreenName12"
+              },
+              {
+                  "Id": 3,
+                  "ScreenName": "ScreenName13"
+              }
+            ]
+        },
+        {
+            "Id": 3,
+            "ModuleName": "ModuleName13",
+            "ScreenList": [
+              {
+                  "Id": 1,
+                  "ScreenName": "ScreenName11"
+              },
+              {
+                  "Id": 2,
+                  "ScreenName": "ScreenName12"
+              }
+            ]
+        },
+        {
+            "Id": 4,
+            "ModuleName": "ModuleName14",
+            "ScreenList": [
+              {
+                  "Id": 1,
+                  "ScreenName": "ScreenName11"
+              },
+              {
+                  "Id": 2,
+                  "ScreenName": "ScreenName12"
+              },
+              {
+                  "Id": 3,
+                  "ScreenName": "ScreenName13"
+              },
+              {
+                  "Id": 4,
+                  "ScreenName": "ScreenName14"
+              }]
+        },
+        {
+            "Id": 5,
+            "ModuleName": "ModuleName15",
+            "ScreenList": [
+              {
+                  "Id": 1,
+                  "ScreenName": "ScreenName11"
+              },
+              {
+                  "Id": 2,
+                  "ScreenName": "ScreenName12"
+              },
+              {
+                  "Id": 3,
+                  "ScreenName": "ScreenName13"
+              }
+            ]
+        },
+        {
+            "Id": 6,
+            "ModuleName": "ModuleName16",
+            "ScreenList": [
+              {
+                  "Id": 1,
+                  "ScreenName": "ScreenName11"
+              }
+            ]
         }
+      ]
+  },
+  {
+      "Id": 2,
+      "Name": "User admin",
+      "ModuleList": [
+        {
+            "Id": 1,
+            "ModuleName": "ModuleName21"
+        },
+        {
+            "Id": 2,
+            "ModuleName": "ModuleName22"
+        },
+        {
+            "Id": 3,
+            "ModuleName": "ModuleName23"
+        }
+      ]
+  },
+  {
+      "Id": 3,
+      "Name": "Manager",
+      "ModuleList": [
+        {
+            "Id": 1,
+            "ModuleName": "ModuleName31"
+        }
+      ]
+  },
+  {
+      "Id": 4,
+      "Name": "Developer",
+      "ModuleList": [
+        {
+            "Id": 1,
+            "ModuleName": "ModuleName41"
+        },
+        {
+            "Id": 2,
+            "ModuleName": "ModuleName42"
+        },
+        {
+            "Id": 3,
+            "ModuleName": "ModuleName43"
+        }
+      ]
+  },
+  {
+      "Id": 5,
+      "Name": "Anonymous",
+      "ModuleList": [
+        {
+            "Id": 1,
+            "ModuleName": "ModuleName51"
+        },
+        {
+            "Id": 3,
+            "ModuleName": "ModuleName53"
+        }
+      ]
+  }
+            ];
+        }
+        console.log(JSON.stringify(vm.RoleList));
     }
 })();

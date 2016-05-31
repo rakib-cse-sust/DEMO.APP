9daefd3bfad3c59456e28f28882f0b41aa57ed02
rakib-cse-sust <rakib.cse.sust@gmail.com>
2016-06-01 01:05:16 +0600

    json update

--- DEMO.APP/Modules/Administration/RoleSetup/controllers/RoleSetupController.js
+++ DEMO.APP/Modules/Administration/RoleSetup/controllers/RoleSetupController.js
@@ -17,55 +17,55 @@
         function activate() {
 
             vm.RoleList = [{
-                "Id": 1, "Name": "Admin", "ModuleList": [{
-                    "Id": 1, "ModuleName": "ModuleName11", "ScreenList":
+                Id: 1, Name: 'Admin', ModuleList: [{
+                    Id: 1, ModuleName: 'ModuleName11', ScreenList:
                         [
-                            { "Id": 1, "ScreenName": "ScreenName11", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'N' },
-                            { "Id": 2, "ScreenName": "ScreenName12", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'R' },
-                            { "Id": 3, "ScreenName": "ScreenName13", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'W' },
-                            { "Id": 4, "ScreenName": "ScreenName14", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'R' },
-                            { "Id": 5, "ScreenName": "ScreenName15", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'W' },
-                            { "Id": 6, "ScreenName": "ScreenName16", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'N' }]
-                }, {
-                    "Id": 2, "ModuleName": "ModuleName12", "ScreenList":
+                            { Id: 1, ScreenName: 'ScreenName11', none: 'N', read: 'R', write: 'W', model: 'N' },
+                            { Id: 2, ScreenName: 'ScreenName12', none: 'N', read: 'R', write: 'W', model: 'R' },
+                            { Id: 3, ScreenName: 'ScreenName13', none: 'N', read: 'R', write: 'W', model: 'W' },
+                            { Id: 4, ScreenName: 'ScreenName14', none: 'N', read: 'R', write: 'W', model: 'R' },
+                            { Id: 5, ScreenName: 'ScreenName15', none: 'N', read: 'R', write: 'W', model: 'W' },
+                            { Id: 6, ScreenName: 'ScreenName16', none: 'N', read: 'R', write: 'W', model: 'N' }]
+                    }, {
+                        Id: 2, ModuleName: 'ModuleName12', ScreenList:
+                            [
+                            { Id: 1, ScreenName: 'ScreenName11', none: 'N', read: 'R', write: 'W', model: 'R' },
+                            { Id: 2, ScreenName: 'ScreenName12', none: 'N', read: 'R', write: 'W', model: 'R' },
+                            { Id: 3, ScreenName: 'ScreenName13', none: 'N', read: 'R', write: 'W', model: 'R' }]
+                    }, {
+                        Id: 3, ModuleName: 'ModuleName13', ScreenList:
+                            [
+                                { Id: 1, ScreenName: 'ScreenName11', none: 'N', read: 'R', write: 'W', model: 'W' },
+                                { Id: 2, ScreenName: 'ScreenName12', none: 'N', read: 'R', write: 'W', model: 'N' }]
+                    }, {
+                        Id: 4, ModuleName: 'ModuleName14', ScreenList:
+                            [
+                                { Id: 1, ScreenName: 'ScreenName11', none: 'N', read: 'R', write: 'W', model: 'R' },
+                                { Id: 2, ScreenName: 'ScreenName12', none: 'N', read: 'R', write: 'W', model: 'R' },
+                                { Id: 3, ScreenName: 'ScreenName13', none: 'N', read: 'R', write: 'W', model: 'W' },
+                                { Id: 4, ScreenName: 'ScreenName14', none: 'N', read: 'R', write: 'W', model: 'N' }]
+                    }
+                    , {
+                        Id: 5, ModuleName: 'ModuleName15', ScreenList:
                         [
-                        { "Id": 1, "ScreenName": "ScreenName11", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                        { "Id": 2, "ScreenName": "ScreenName12", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                        { "Id": 3, "ScreenName": "ScreenName13", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' }]
-                }, {
-                    "Id": 3, "ModuleName": "ModuleName13", "ScreenList":
-                        [
-                            { "Id": 1, "ScreenName": "ScreenName11", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                            { "Id": 2, "ScreenName": "ScreenName12", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' }]
-                }, {
-                    "Id": 4, "ModuleName": "ModuleName14", "ScreenList":
-                        [
-                            { "Id": 1, "ScreenName": "ScreenName11", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                            { "Id": 2, "ScreenName": "ScreenName12", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                            { "Id": 3, "ScreenName": "ScreenName13", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                            { "Id": 4, "ScreenName": "ScreenName14", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' }]
+                            { Id: 1, ScreenName: 'ScreenName11', none: 'N', read: 'R', write: 'W', model: 'R' },
+                            { Id: 2, ScreenName: 'ScreenName12', none: 'N', read: 'R', write: 'W', model: 'N' },
+                            { Id: 3, ScreenName: 'ScreenName13', none: 'N', read: 'R', write: 'W', model: 'W' }]
+                    }, {
+                        Id: 6, ModuleName: 'ModuleName16', ScreenList:
+                            [
+                                { Id: 1, ScreenName: 'ScreenName11', none: 'N', read: 'R', write: 'W', model: 'R' }]
+                    }]
+                }
+
+                , {
+                    Id: 2, Name: 'Admin developer'
                 }
                 , {
-                    "Id": 5, "ModuleName": "ModuleName15", "ScreenList":
-                    [
-                        { "Id": 1, "ScreenName": "ScreenName11", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                        { "Id": 2, "ScreenName": "ScreenName12", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' },
-                        { "Id": 3, "ScreenName": "ScreenName13", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' }]
-                }, {
-                    "Id": 6, "ModuleName": "ModuleName16", "ScreenList":
-                        [
-                            { "Id": 1, "ScreenName": "ScreenName11", 'none': 'N', 'read': 'R', 'write': 'W', 'model': 'first' }]
-                }]
-            }
+                    Id: 3, Name: 'Admin manager'
+                }
 
-            , {
-                "Id": 2, "Name": "Admin developer"
-            }
-            , {
-                "Id": 3, "Name": "Admin manager"
-            }
-
-            ];
+                ];
         }
 
     }
